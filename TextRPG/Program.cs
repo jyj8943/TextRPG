f576c78937be8e2875d554bd2754a709ce272a3f
jyj8943 <jyj8943@gmail.com>
2025-07-10 17:46:03 +0900

    게임 시작 화면 제작
    StartMenu 메소드 제작

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -29,6 +29,8 @@
         static void Main(string[] args)
         {
             GameStart();
+
+            StartMenu();
         }
 
         static private void GameStart()
@@ -39,9 +41,10 @@
 
             do
             {
-                Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+                Console.WriteLine("텍스트 RPG 스파르타 던전에 오신 여러분 환영합니다.");
                 Console.WriteLine("원하시는 이름을 설정해주세요. \n");
                 string userInput = Console.ReadLine();
+                isInput = false;
 
                 // 1번 저장, 2번 다시 입력 => 그 외에는 다시 물어보는 함수 작성해야함
                 Console.WriteLine("\n입력하신 이름은 {0}입니다.",  userInput);
@@ -84,6 +87,7 @@
                     else if (saveInput == 2)
                     {
                         Console.WriteLine("\n이름을 취소합니다.");
+                        isInput = true;
                     }
                     else
                     {
@@ -91,6 +95,71 @@
                     }
                 } while (!isInput);
             } while (!isSave);
+        }
+
+        static private void StartMenu()
+        {
+            int userInput = -1;
+            bool isSelect = false;
+            Console.Clear(); // 게임스타트 메소드 화면 정리
+
+
+            do
+            {
+                Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+                Console.WriteLine("이곳에서 던전으로 들어가기 전 다양한 활동을 할 수 있습니다.");
+                Console.WriteLine("\n■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+                Console.WriteLine("\n1. 상태 보기");
+                Console.WriteLine("2. 인벤토리");
+                Console.WriteLine("3. 상점");
+                Console.WriteLine("\n■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+                Console.WriteLine("\n원하시는 행동을 입력해주세요.");
+                Console.Write(">>> ");
+
+                try
+                {
+                    var input = Console.ReadLine();
+
+                    // 입력이 null이거나 공백일 경우 처리
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+                        Console.WriteLine("\n입력이 비어 있습니다.");
+                    }
+                    else
+                    {
+                        userInput = int.Parse(input);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\n정수를 입력해 주세요.");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("\n입력한 숫자가 너무 큽니다.");
+                }
+
+                if (userInput == 1)
+                {
+                    Console.WriteLine("\n상태 보기를 선택하셨습니다.");
+                    isSelect = true;
+                }
+                else if (userInput == 2)
+                {
+                    Console.WriteLine("\n인벤토리를 선택하셨습니다.");
+                    isSelect = true;
+                }
+                else if (userInput == 3)
+                {
+                    Console.WriteLine("\n상점을 선택하셨습니다.");
+                    isSelect = true;
+                }
+                else
+                {
+                    Console.WriteLine("\n잘못된 숫자를 입력하셨습니다.\n");
+                }
+            } while (!isSelect);
+
         }
     }
 }
